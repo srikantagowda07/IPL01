1b7dcee84eca67e83625928ef0a58d53280d5c64
srikanta gowda <srikanta.gowda@mountblue.tech>
2020-07-30 08:49:04 +0530

    Add HIGHCAHRTS for Strike-Rate of Each YEAR for the top performing players

--- public/app2.js
+++ public/app2.js
@@ -49,3 +49,55 @@ function visualizeToss_MatchWinners(Toss_MatchWinners) {
       ]
     });
   }
+
+  
+function visualstrike_Rate(strike_Rate, man_of_The_Match) {
+    const teams = Object.values(man_of_The_Match);
+    const seasons=Object.keys(strike_Rate);
+    let seriesData=[];
+    seriesData = teams.map(team => ({
+      name: team,
+      data: seasons.map(season => strike_Rate[season][team] || 0)
+    }));
+  
+    Highcharts.chart("strike_Rate", {
+      chart: {
+          type: 'column'
+      },
+      title: {
+          text: 'STRIKE RATE OF TOP MAN OF THE MATCH PLAYERS'
+      },
+      subtitle: {
+        text:
+          'Source: <a href="https://www.kaggle.com/nowke9/ipldata/data">IPL Dataset</a>'
+      },
+      xAxis: {
+        title: {
+          text: "Teams"
+        },
+          categories: seasons,
+          crosshair: true
+      },
+      yAxis: {
+          min: 0,
+          title: {
+              text: 'strike_Rate'
+          }
+      },
+      tooltip: {
+          headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+          pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+              '<td style="padding:0"><b>{point.y}</b></td></tr>',
+          footerFormat: '</table>',
+          shared: true,
+          useHTML: true
+      },
+      plotOptions: {
+          column: {
+              pointPadding: 0.2,
+              borderWidth: 0
+          }
+      },
+      series: seriesData
+  });
+  }
